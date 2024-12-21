0bf5089fdd32bc87cce36987ce8117806fc917e3
Pedro Araújo <paaraujo.ti@gmail.com>
2024-12-21 08:40:05 -0300

    update DeltaModelMessages

--- src/DeltaModelMessages.pas
+++ src/DeltaModelMessages.pas
@@ -5,7 +5,20 @@ unit DeltaModelMessages;
 interface
 
 resourcestring
+  //Model
   DeltaModelClassNotAssigned = 'DeltaModelClass not assigned to DeltaModelList.';
+
+  //Validator
+  ValidationFailedForField = 'Validation failed for field %s: %s';
+  ValueCannotBeEmpty = 'Value cannot be empty.';
+  MinimumLenght = 'Minimum length: %d characters.';
+  MaximumLenght = 'Maximum length: %d characters.';
+  ValueDoesNotMatchREGEX = 'Value does not match the expected pattern.';
+  InvalidEmail = 'Invalid email.';
+  MinimumAllowedValue = 'Minimum allowed value: %.2f.';
+  MaximumAllowedValue = 'Maximum allowed value: %.2f.';
+  ValueMustBeGreaterThanZero = 'Value must be greater than zero.';
+  AllowedRange = 'Allowed range: %s to %s.';
 
 implementation
 
