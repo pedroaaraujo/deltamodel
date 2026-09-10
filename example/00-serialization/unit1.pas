037e272f3da1ecb986ed2a44054590ccf8964b1f
Pedro Araújo <paaraujo.ti@gmail.com>
2026-09-10 20:03:53 -0300

    Aprimoramento do módulo ORM e outros ajustes

--- example/00-serialization/unit1.pas
+++ example/00-serialization/unit1.pas
@@ -5,26 +5,50 @@ unit Unit1;
 interface
 
 uses
-  DeltaModel, DeltaModel.Types, DeltaValidator,
-
+  DeltaModel, DeltaModel.Fields, DeltaValidator,
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls,
-  ExtCtrls, Spin, Variants;
+  ExtCtrls, Spin, Variants, fpjson;
 
 type
 
-  { TUser }
+  { TAddress - Modelo aninhado de endereço }
+
+  TAddress = class(TDeltaModel)
+  private
+    Fcity:    TDFStringRequired;
+    Fstate:   TDFStringRequired;
+    Fzipcode: TDFStringNull;
+  published
+    property city:    TDFStringRequired read Fcity    write Fcity;
+    property state:   TDFStringRequired read Fstate   write Fstate;
+    property zipcode: TDFStringNull     read Fzipcode write Fzipcode;
+  end;
+
+  { TUser - Modelo principal com nested model e UUID }
 
   TUser = class(TDeltaModel)
   private
-    Fage: TDFIntNull;
-    Fname: TDFStringRequired;
+    Fid:        TDFUUIDNull;
+    Fname:      TDFStringRequired;
+    Femail:     TDFStringRequired;
+    Fage:       TDFIntNull;
+    Fscore:     TDFDoubleNull;
+    Factive:    TDFBooleanRequired;
+    Fcreated:   TDFDateTimeNull;
+    Faddress:   TAddress;
   published
-    property name: TDFStringRequired read Fname write Fname;
-    property age: TDFIntNull read Fage write Fage;
+    property id:      TDFUUIDNull      read Fid      write Fid;
+    property name:    TDFStringRequired read Fname    write Fname;
+    property email:   TDFStringRequired read Femail   write Femail;
+    property age:     TDFIntNull        read Fage     write Fage;
+    property score:   TDFDoubleNull     read Fscore   write Fscore;
+    property active:  TDFBooleanRequired read Factive write Factive;
+    property created: TDFDateTimeNull   read Fcreated write Fcreated;
+    property address: TAddress          read Faddress write Faddress;
   public
-    procedure Validate;
+    procedure Configure; override;
+    procedure Validate; override;
   end;
-
 
   { TForm1 }
 
@@ -32,28 +56,37 @@ type
     Button1: TButton;
     Button2: TButton;
     Button3: TButton;
+    Button4: TButton;
     edtDeserializationAge: TSpinEdit;
     edtValidationAge: TSpinEdit;
     edtSerializationName: TEdit;
     edtDeserializationName: TEdit;
     edtValidationName: TEdit;
+    edtSerializationEmail: TEdit;
+    edtValidationEmail: TEdit;
+    edtDeserializationEmail: TEdit;
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
     Label5: TLabel;
     Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
     mmoSerialization: TMemo;
     mmoDeserialization: TMemo;
+    mmoSchema: TMemo;
     pgcBase: TPageControl;
     edtSerializationAge: TSpinEdit;
     tbsValidation: TTabSheet;
     tbsSerialize: TTabSheet;
     tbsDeserialization: TTabSheet;
+    tbsSchema: TTabSheet;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
-    procedure edtSerializationAge1Change(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
   private
 
@@ -70,30 +103,58 @@ implementation
 
 { TUser }
 
+procedure TUser.Configure;
+begin
+  // Define o tamanho máximo do campo email
+  Self.email.Size := 200;
+  Self.name.Size  := 120;
+
+  // Define campos invisíveis na serialização (ex: campo interno)
+  // Self.someInternalField.Visible := False;
+end;
+
 procedure TUser.Validate;
 var
-  Result: TValid;
+  VResult: TValid;
 begin
+  // Chama validação de campos Required (IsNull check)
+  inherited Validate;
+
   Self.Validator.Clear;
 
+  // Validações do nome
   Self.Validator
     .AddField('name', Self.name.Value)
+    .AddValidator(TValidatorItemNotEmpty.Create)
     .AddValidator(TValidatorItemMinLength.Create(2))
-    .AddValidator(TValidatorItemMaxLength.Create(60));
+    .AddValidator(TValidatorItemMaxLength.Create(120));
 
+  // Validação de email
   Self.Validator
-    .AddField('age', Self.age.Value)
-    .AddValidator(TValidatorItemMinValue.Create(0));
+    .AddField('email', Self.email.Value)
+    .AddValidator(TValidatorItemNotEmpty.Create)
+    .AddValidator(TValidatorEmail.Create);
 
-  Result := Self.Validator.Validate;
-
-  if Result.OK then
+  // Validação de idade (campo opcional, só valida se preenchido)
+  if not Self.age.IsNull then
   begin
-    ShowMessage('Validated');
-    Exit;
+    Self.Validator
+      .AddField('age', Self.age.Value)
+      .AddValidator(TValidatorItemMinValue.Create(0))
+      .AddValidator(TValidatorItemMaxValue.Create(150));
   end;
 
-  ShowMessage(Result.Message);
+  // Validação de score (campo opcional)
+  if not Self.score.IsNull then
+  begin
+    Self.Validator
+      .AddField('score', Self.score.Value)
+      .AddValidator(TValidatorItemBetween.Create(0, 10));
+  end;
+
+  VResult := Self.Validator.Validate;
+  if not VResult.OK then
+    raise EDeltaValidation.Create(VResult.Message);
 end;
 
 { TForm1 }
@@ -103,20 +164,32 @@ begin
   pgcBase.ActivePageIndex := 0;
 end;
 
+// Serialização: preenche um TUser com dados e exibe o JSON resultante
 procedure TForm1.Button1Click(Sender: TObject);
 var
   User: TUser;
 begin
   User := TUser.Create;
   try
-    User.name.Value := edtSerializationName.Text;
-    User.age.Value  := edtSerializationAge.Value;
+    User.name.Value    := edtSerializationName.Text;
+    User.email.Value   := edtSerializationEmail.Text;
+    User.age.Value     := edtSerializationAge.Value;
+    User.active.Value  := True;
+    User.score.Value   := 8.5;
+    User.created.Value := Now;
+
+    // Endereço aninhado
+    User.address.city.Value    := 'São Paulo';
+    User.address.state.Value   := 'SP';
+    User.address.zipcode.Value := '01310-100';
+
     mmoSerialization.Lines.Text := User.ToJson;
   finally
     User.Free;
   end;
 end;
 
+// Desserialização: lê JSON do memo e preenche os campos
 procedure TForm1.Button2Click(Sender: TObject);
 var
   User: TUser;
@@ -124,31 +197,49 @@ begin
   User := TUser.Create;
   try
     User.FromJson(mmoDeserialization.Lines.Text);
-    edtDeserializationName.Text := User.name.Value;
-    edtDeserializationAge.Value := User.age.Value;
+    edtDeserializationName.Text  := User.name.Value;
+    edtDeserializationEmail.Text := User.email.Value;
+    if not User.age.IsNull then
+      edtDeserializationAge.Value := User.age.AsInteger
+    else
+      edtDeserializationAge.Value := 0;
   finally
     User.Free;
   end;
 end;
 
+// Validação: valida os dados e exibe resultado
 procedure TForm1.Button3Click(Sender: TObject);
 var
   User: TUser;
 begin
   User := TUser.Create;
   try
-    User.name.Value := edtValidationName.Text;
-    User.age.Value  := edtValidationAge.Value;
-    User.Validate;
+    User.name.Value   := edtValidationName.Text;
+    User.email.Value  := edtValidationEmail.Text;
+    User.age.Value    := edtValidationAge.Value;
+    User.active.Value := True;
+
+    // Endereço obrigatório para o exemplo
+    User.address.city.Value  := 'Campinas';
+    User.address.state.Value := 'SP';
+
+    try
+      User.Validate;
+      ShowMessage('✓ Dados válidos!');
+    except
+      on E: EDeltaValidation do
+        ShowMessageFmt('✗ Erro de validação:%s%s', [sLineBreak, E.Message]);
+    end;
   finally
     User.Free;
   end;
 end;
 
-procedure TForm1.edtSerializationAge1Change(Sender: TObject);
+// Schema: gera o Swagger/OpenAPI schema do modelo
+procedure TForm1.Button4Click(Sender: TObject);
 begin
-
+  mmoSchema.Lines.Text := TUser.SwaggerSchema(False);
 end;
 
 end.
-
--- example/01-database/unit1.pas
+++ example/01-database/unit1.pas
@@ -6,52 +6,69 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls,
-  DeltaModel, DeltaModel.Fields, DeltaModel.SQLBuilder, Variants,
-  DeltaModel.ORM.Connection, DeltaModel.ORM.Types, DeltaModel.ORM.Schema;
+  ExtCtrls, Variants,
+  DeltaModel, DeltaModel.Fields, DeltaModel.SQLBuilder,
+  DeltaModel.ORM.Connection, DeltaModel.ORM.Types, DeltaModel.ORM.Schema,
+  DeltaModel.ORM.DDL, DeltaModel.ORM.DML;
 
 type
 
-  { TPerson }
+  { TPerson - Modelo de teste com AutoInc e tipos variados }
 
   TPerson = class(TDeltaModel)
   private
-    Fage: TDFIntRequired;
     Fid: TDFIntNull;
-    Flimit: TDFCurrencyRequired;
     Fname: TDFStringRequired;
     Fsurname: String;
+    Fage: TDFIntRequired;
+    FcreditLimit: TDFCurrencyRequired;
+    Factive: TDFBooleanRequired;
+    Fcreated: TDFDateTimeNull;
   published
     property id: TDFIntNull read Fid write Fid;
     property name: TDFStringRequired read Fname write Fname;
     property surname: String read Fsurname write Fsurname;
     property age: TDFIntRequired read Fage write Fage;
-    property creditLimit: TDFCurrencyRequired read Flimit write Flimit;
+    property creditLimit: TDFCurrencyRequired read FcreditLimit write FcreditLimit;
+    property active: TDFBooleanRequired read Factive write Factive;
+    property created: TDFDateTimeNull read Fcreated write Fcreated;
   public
     procedure AfterConstruction; override;
+    procedure Configure; override;
   end;
 
   { TForm1 }
 
   TForm1 = class(TForm)
     Button2: TButton;
+    btnTestCRUD: TButton;
+    cmbDialect: TComboBox;
     Edit1: TEdit;
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
     Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
     mmoDDL: TMemo;
+    mmoDDLPreview: TMemo;
     mmoDelete: TMemo;
     mmoInsert: TMemo;
     mmoSelect: TMemo;
     mmoUpdate: TMemo;
     PageControl1: TPageControl;
+    pnlTop: TPanel;
     tbsSQL: TTabSheet;
     TabSheet2: TTabSheet;
     procedure Button2Click(Sender: TObject);
+    procedure btnTestCRUDClick(Sender: TObject);
+    procedure cmbDialectChange(Sender: TObject);
     procedure FormShow(Sender: TObject);
   private
-    procedure BuildSQL;
+    function CurrentDialect: TDatabaseDialect;
+    procedure BuildSQL(ADialect: TDatabaseDialect);
   public
 
   end;
@@ -68,15 +85,98 @@ implementation
 procedure TPerson.AfterConstruction;
 begin
   inherited AfterConstruction;
-  Self.id.DBOptions := [dboPrimaryKey];
+  Self.id.DBOptions := [dboPrimaryKey, dboAutoInc];
+end;
+
+procedure TPerson.Configure;
+begin
+  Self.name.Size := 120;
 end;
 
 { TForm1 }
 
+function TForm1.CurrentDialect: TDatabaseDialect;
+begin
+  case cmbDialect.ItemIndex of
+    0: Result := ddPostgreSQL;
+    1: Result := ddMySQL;
+    2: Result := ddSQLite;
+    3: Result := ddFirebird;
+    4: Result := ddMSSQL;
+    5: Result := ddOracle;
+  else
+    Result := ddPostgreSQL;
+  end;
+end;
+
 procedure TForm1.FormShow(Sender: TObject);
 begin
   PageControl1.ActivePageIndex := 0;
-  BuildSQL;
+  cmbDialect.ItemIndex := 0;
+  BuildSQL(CurrentDialect);
+end;
+
+procedure TForm1.cmbDialectChange(Sender: TObject);
+begin
+  BuildSQL(CurrentDialect);
+end;
+
+procedure TForm1.BuildSQL(ADialect: TDatabaseDialect);
+var
+  Person: TPerson;
+  Builder: TDMSQLBuilder;
+begin
+  Person := TPerson.Create;
+  try
+    // 1. SELECT com paginação (Page 2, tamanho 10)
+    Builder := TDMSQLBuilder.Create(Person, ADialect);
+    try
+      mmoSelect.Lines.Text := Builder
+        .Select
+        .Where('active = 1')
+        .OrderBy('name ASC')
+        .Page(2, 10)
+        .Build;
+    finally
+      Builder.Free;
+    end;
+
+    // 2. INSERT com Returning / Output
+    Person.name.Value := 'Carlos Eduardo';
+    Person.surname := 'Silveira';
+    Person.age.Value := 35;
+    Person.creditLimit.Value := 12500.50;
+    Person.active.Value := True;
+    Person.created.Value := Now;
+
+    mmoInsert.Lines.Text := TDMSQLBuilder.CreateInsertReturning(
+      Person,
+      ADialect,
+      True
+    );
+
+    // 3. UPDATE
+    Person.id.Value := 42;
+    mmoUpdate.Lines.Text := TDMSQLBuilder.CreateUpdate(
+      Person,
+      ADialect
+    );
+
+    // 4. DELETE
+    mmoDelete.Lines.Text := TDMSQLBuilder.CreateDelete(
+      Person,
+      ADialect
+    );
+
+    // 5. DDL gerado para o dialeto
+    mmoDDLPreview.Lines.Text := TDDLBuilder.CreateTableAndFields(
+      Person,
+      nil,
+      ADialect
+    );
+  finally
+    Person.Free;
+  end;
 end;
 
 procedure TForm1.Button2Click(Sender: TObject);
@@ -85,62 +185,124 @@ var
   Schema: TDeltaORMSchema;
 begin
   Con := TDeltaORMEngine.Create(Edit1.Text);
-  Schema := TDeltaORMSchema.Create(Con);
   try
+    Schema := TDeltaORMSchema.Create(Con);
     try
-      Con.Connection.Open;
-      ShowMessage('Connected');
+      try
+        Con.Connection.Open;
+        mmoDDL.Lines.Add('=== Conectado com sucesso via: ' + Con.Connection.ConnectorType + ' ===');
+        mmoDDL.Lines.Add('Dialeto ativo: ' + TDatabaseDialectHelper.ToString(Con.Dialect));
 
+        Schema.RegisterModel(TPerson.Create);
+        Schema.PrepareDB(True);
 
-      Schema.RegisterModel(TPerson.Create);
-      Schema.PrepareDB(True);
-      mmoDDL.Lines.Text := Schema.SQL.Text;
-    finally
-      Schema.Free;
+        mmoDDL.Lines.Add('--- DDL Executado ---');
+        mmoDDL.Lines.Add(Schema.SQL.Text);
+        ShowMessage('Schema preparado com sucesso!');
+      finally
+        Schema.Free;
+      end;
+    except
+      on E: Exception do
+        ShowMessageFmt('Falha ao preparar schema:%s%s', [sLineBreak, E.Message]);
     end;
-  except
-    on E: Exception do
-    begin
-      ShowMessageFmt('Failed.%s%s', [sLineBreak, E.Message]) ;
-    end;
+  finally
+    Con.Free;
   end;
 end;
 
-procedure TForm1.BuildSQL;
+procedure TForm1.btnTestCRUDClick(Sender: TObject);
 var
-  Person: TPerson;
+  Con: TDeltaORMEngine;
+  Schema: TDeltaORMSchema;
+  P, FoundP: TPerson;
+  List: TDeltaModelList;
+  I: Integer;
+  Total: Int64;
 begin
-  Person := TPerson.Create;
+  mmoDDL.Clear;
+  mmoDDL.Lines.Add('=== Iniciando Teste de Operações ORM ===');
+
+  Con := TDeltaORMEngine.Create(Edit1.Text);
   try
-    mmoSelect.Lines.Text := TDMSQLBuilder.CreateSelect(
-      Person,
-      ddFirebird,
-      'id = 1'
-    );
+    try
+      Con.Connection.Open;
+      mmoDDL.Lines.Add('1. Conectado ao banco (' + TDatabaseDialectHelper.ToString(Con.Dialect) + ')');
 
-    Person.name.Value := 'No '' Name';
-    Person.age.Value := 30;
-    Person.creditLimit.Value := 399999.9999;
-    mmoInsert.Lines.Text := TDMSQLBuilder.CreateInsert(
-      Person,
-      ddFirebird
-    );
+      // 2. Prepara tabela
+      Schema := TDeltaORMSchema.Create(Con);
+      try
+        Schema.RegisterModel(TPerson.Create);
+        Schema.PrepareDB(True);
+        mmoDDL.Lines.Add('2. Tabela person criada/verificada.');
+      finally
+        Schema.Free;
+      end;
 
-    Person.name.Value := 'New Name';
-    Person.id.Value := 1;
-    mmoUpdate.Lines.Text := TDMSQLBuilder.CreateUpdate(
-      Person,
-      ddFirebird
-    );
+      // 3. Teste do Save (Insert automático porque PK está vazia)
+      P := TPerson.Create;
+      try
+        P.name.Value := 'Ana Beatriz';
+        P.surname := 'Lima';
+        P.age.Value := 28;
+        P.creditLimit.Value := 8000;
+        P.active.Value := True;
+        P.created.Value := Now;
 
-    mmoDelete.Lines.Text := TDMSQLBuilder.CreateDelete(
-      Person,
-      ddFirebird
-    );
+        if Con.Save(P) then
+          mmoDDL.Lines.Add('3. [SAVE - INSERT] Registro inserido com sucesso: ' + P.name.Value);
+      finally
+        P.Free;
+      end;
+
+      // 4. Inserindo segundo registro
+      P := TPerson.Create;
+      try
+        P.name.Value := 'Bruno Martins';
+        P.surname := 'Rocha';
+        P.age.Value := 40;
+        P.creditLimit.Value := 15000;
+        P.active.Value := True;
+        P.created.Value := Now;
+
+        Con.Insert(P);
+        mmoDDL.Lines.Add('4. [INSERT] Segundo registro inserido: ' + P.name.Value);
+      finally
+        P.Free;
+      end;
+
+      // 5. Contagem com Con.Count
+      Total := Con.Count(TPerson);
+      mmoDDL.Lines.Add(Format('5. [COUNT] Total de pessoas no banco: %d', [Total]));
+
+      // 6. Consulta fluente com Query e paginação
+      List := Con.Query(TPerson)
+        .OrderBy('name ASC')
+        .Page(1, 10)
+        .All;
+      try
+        mmoDDL.Lines.Add('6. [QUERY.ALL] Registros retornados pela consulta:');
+        for I := 0 to Pred(List.Records.Count) do
+        begin
+          FoundP := List.Records[I] as TPerson;
+          mmoDDL.Lines.Add(Format('   [%d] Nome: %s, Idade: %d, Limite: R$ %.2f',
+            [I + 1, FoundP.name.Value, FoundP.age.AsInteger, FoundP.creditLimit.AsCurrency]));
+        end;
+      finally
+        List.Free;
+      end;
+
+      ShowMessage('Teste de CRUD executado com sucesso! Veja os logs no memo.');
+    except
+      on E: Exception do
+      begin
+        mmoDDL.Lines.Add('ERRO: ' + E.Message);
+        ShowMessageFmt('Erro durante execução do CRUD:%s%s', [sLineBreak, E.Message]);
+      end;
+    end;
   finally
-    Person.Free;
+    Con.Free;
   end;
 end;
 
 end.
-
